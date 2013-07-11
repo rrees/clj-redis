c648f4aea0f538e0cf53bea5298ee483a6566a01
Robert Rees <shudderfix@gmail.com>
2013-07-11 09:06:29 +0100

    Changes dependencies to simply focus on the latest versions of Clojure and Redis

--- project.clj
+++ project.clj
@@ -1,10 +1,5 @@
 (defproject clj-redis "0.0.13-SNAPSHOT"
   :description "Clojure Redis client library"
   :url "https://github.com/mmcgrana/clj-redis"
-  :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
-                 [redis.clients/jedis "1.5.2"]]
-  :multi-deps {"1.3" [[org.clojure/clojure "1.3.0"]
-                      [redis.clients/jedis "1.5.2"]]
-               "1.2" [[org.clojure/clojure "1.2.1"]
-                      [redis.clients/jedis "1.5.2"]]}
-  :dev-deps [[lein-multi "1.0.0"]])
+  :dependencies [[org.clojure/clojure "[1.5.1]"]
+                 [redis.clients/jedis "2.0.0"]])
